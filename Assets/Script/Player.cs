f6b5dde62e9128df8cdc4a452265195c03c5efc4
408-17\student <fca>
2024-10-31 19:46:58 +0900

    カメラ追従など

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //歩行速度
+    private float speed = 0.03f;
+    //回転速度
+    private float rotateSpeed = 0.3f;
+    //垂直、水平
+    private float horizonalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        horizonalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //入力があれば移動
+        if(verticalInput != 0 )
+        {
+            transform.position += transform.forward * speed * verticalInput;
+        }
+
+        //回転
+        transform.Rotate(new Vector3(0.0f, rotateSpeed * horizonalInput, 0.0f));
+    }
+}
